d4f36b16897269e474631871df0febd0e8b229a3
Liliia <liliia.pohorielova@nure.ua>
2022-03-19 23:25:15 +0200

    Add SQL Show

---
+++ src/main/java/ua/com/alevel/util/ConsoleLoggerSQL.java
@@ -0,0 +1,11 @@
+package ua.com.alevel.util;
+
+public class ConsoleLoggerSQL {
+
+    public static void logMethod(String nameOfMethod) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(nameOfMethod + " ------------------------------------------------------------------------------");
+    }
+}
