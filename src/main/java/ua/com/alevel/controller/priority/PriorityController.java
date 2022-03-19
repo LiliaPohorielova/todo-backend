fa05f0c4584c8441ba0392c82d681055b17b1d90
Liliia <liliia.pohorielova@nure.ua>
2022-03-19 23:27:00 +0200

    Change Priority

--- src/main/java/ua/com/alevel/controller/priority/PriorityController.java
+++ src/main/java/ua/com/alevel/controller/priority/PriorityController.java
@@ -1,12 +1,16 @@
 package ua.com.alevel.controller.priority;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ua.com.alevel.entity.Priority;
 import ua.com.alevel.repository.priority.PriorityRepository;
+import ua.com.alevel.search.PrioritySearchValues;
+import ua.com.alevel.util.ConsoleLoggerSQL;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/priority")
@@ -20,8 +24,88 @@ public class PriorityController {
         this.priorityRepository = priorityRepository;
     }
 
-    @GetMapping("/test")
-    public List<Priority> test() {
-        return priorityRepository.findAll();
+    @GetMapping("/all")
+    public List<Priority> findAll() {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: findAll()");
+        // return priorityRepository.findAll();
+        return priorityRepository.findAllByOrderByIdAsc();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Priority> addPriority(@RequestBody Priority priority) {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: addPriority()");
+        // Id нового объекта должен быть пустым
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("ID of new priority must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // Название нового объекта не должно быть пустым
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Priority name must not be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // Цвет нового объекта не должно быть пустым
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Priority color must not be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Priority> editPriority(@RequestBody Priority priority) {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: editPriority()");
+        // Id редактированного объекта НЕ должен быть пустым
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("ID of edit priority must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // Название редактированного объекта не должно быть пустым
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Priority name must not be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // Цвет редактированного объекта не должно быть пустым
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Priority color must not be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // Метод save() работает как на создание так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: findById()");
+        Priority priority = null;
+        try {
+            priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity("Priority with id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priority);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id) {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: deleteById()");
+        try {
+            priorityRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<>("Priority with id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity<>("Priority with id=" + id + " was deleted", HttpStatus.OK);
+    }
+
+    // поиск по любым параметрам
+    // отправляем JSON и по нем будем искать
+    // чтобы не перечислять все параметры для поиска через запятую
+    @PostMapping("/search")
+    public ResponseEntity<List<Priority>> searchPriorities(@RequestBody PrioritySearchValues prioritySearchValues) {
+        ConsoleLoggerSQL.logMethod("PriorityRepository: searchPriorities()");
+        // если не найдется ничего - будут показаны все категории
+        return ResponseEntity.ok(priorityRepository.findByTitle(prioritySearchValues.getTitle()));
     }
 }
--- src/main/java/ua/com/alevel/repository/priority/PriorityRepository.java
+++ src/main/java/ua/com/alevel/repository/priority/PriorityRepository.java
@@ -1,8 +1,21 @@
 package ua.com.alevel.repository.priority;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ua.com.alevel.entity.Category;
 import ua.com.alevel.entity.Priority;
 
+import java.util.List;
+
 @Repository
-public interface PriorityRepository extends JpaRepository<Priority, Long> { }
+public interface PriorityRepository extends JpaRepository<Priority, Long> {
+
+    @Query( "SELECT p FROM Priority p WHERE " +
+            "(:title is null or :title='' or lower(p.title) like lower(concat('%',:title,'%'))) " +
+            "ORDER BY p.title ASC")
+    List<Priority> findByTitle(@Param("title") String title);
+
+    List<Priority> findAllByOrderByIdAsc();
+}
